4e8e54f02c02a0fd9386abcd0b918db8a53e8bf0
Charlie-GCCIS <mcw2518@rit.edu>
2025-10-11 16:10:10 -0400

    Made a new scene

---
+++ Scream Jam/Assets/Scripts/Movement/Vehicle.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Vehicle : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 5f;
+
+    [SerializeField]
+    Vector2 velocity;
+
+    [SerializeField]
+    Vector2 moveDirection;
+
+    [SerializeField]
+    Rigidbody2D rb;
+
+    [SerializeField]
+    Vector2 position;
+
+    int score = 0;
+
+    Vector2 screenBounds;
+
+    public int Score { get { return score; } set { score = value; } }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        position = rb.transform.position;
+
+        //how to calculate the camera screen bounds
+        screenBounds.y = Camera.main.orthographicSize;
+        screenBounds.x = screenBounds.y * Camera.main.aspect;
+
+        score = 0;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (moveDirection.magnitude > 0)
+        {
+            velocity = moveDirection * speed * Time.fixedDeltaTime;
+            Vector2 newPos = (Vector2)transform.position + velocity;
+            rb.MovePosition(newPos);
+        }
+        
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(Vector2.zero, moveDirection);
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + rb.linearVelocity);
+    }
+}
