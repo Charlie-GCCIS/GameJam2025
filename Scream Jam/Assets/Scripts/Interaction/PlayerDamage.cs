4ed4c23db6bc992f2b6584a3b5030fcb5288c575
Julian <jah3290@g.rit.edu>
2025-10-12 18:04:37 -0400

    added "hearts" UI

--- Scream Jam/Assets/Scripts/Interaction/PlayerDamage.cs
+++ Scream Jam/Assets/Scripts/Interaction/PlayerDamage.cs
@@ -1,20 +1,65 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerDamage : MonoBehaviour
 {
     [SerializeField]
     float playerhealth = 100;
+    [SerializeField]
+    int maxHealh = 100;
+
+
+    public Sprite emptyHeart;
+    public Sprite fulHeart;
+    public Image[] hearts;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i <playerhealth)
+            {
+                hearts[i].sprite = fulHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+
+            if (i < maxHealh)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+        if(playerhealth < 75)
+        {
+            hearts[3].sprite = emptyHeart;
+        }
+        if (playerhealth < 50)
+        {
+            hearts[2].sprite = emptyHeart;
+        }
+        if (playerhealth < 25)
+        {
+            hearts[1].sprite = emptyHeart;
+        }
+
+
         if(playerhealth <=0)
         {
             SceneManager.LoadScene("DeathScreen");
